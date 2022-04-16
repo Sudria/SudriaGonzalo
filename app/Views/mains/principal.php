5b6faa117ac6810c5c85c49263a9d3267b8ca550
Sudria <sudria.gonzalo@gmail.com>
2022-04-16 16:28:51 -0300

    Pagina de inicio

---
+++ app/Views/mains/principal.php
@@ -0,0 +1,28 @@
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12 " id="principal">
+            <h1 class=" text-center my-5 text-light" id="titulo"> ESPINNITAS</h1>
+            <p class="text-light text-center mb-5" id="especialistas"> ESPECIALISTAS EN PLANTAS</p>
+            <div class="d-flex justify-content-center mb-5">
+                <button class="btn boton-marron ">Comprar online</button>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-6 d-flex align-items-center ">
+        <p class="texto-base text-center">
+               <b class="fs-1"> QUIÉNES SOMOS</b> <br>  
+                ESPINNITAS nace hace más de 10 años en la Ciudad de Corrientes Capital, Argentina.
+                Desde ese momento nos dedicamos al cultivo y asesoramiento profesional en Cactus, Suculentas y Plantas
+                Exóticas.
+                En la actualidad contamos con tres invernáculos donde producimos nuestras plantas.
+                 </p>
+        </div>
+        <div class="col-6">
+            <img src="img/hoja.jpg" class="img-fluid" alt="hoja">
+            <img src="img/cactus1.jpg" class="img-fluid" alt="cactus">
+        </div>
+    </div>
+</div>
--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -12,8 +12,9 @@
 
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg fixed-top navbar-light texto-base" id="navbar">
+            <div class="container">
+            <a class="navbar-brand texto-base" href="#">ESPINNITAS</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation">
@@ -22,7 +23,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Principal</a>
+                            <a class="nav-link" href="#">Principal</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Quienes Somos</a>
@@ -38,11 +39,12 @@
                         </li>
                     </ul>
                     <form class="d-flex">
-                        <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Buscar">
-                        <button class="btn btn-outline-light" type="submit">Buscar</button>
+                        <input class="form-control me-2" id="input-buscar" type="search" placeholder="Buscar" aria-label="Buscar">
+                        <button class="btn boton-gris" type="submit">Buscar</button>
                     </form>
                 </div>
             </div>
         </nav>
     </header>
     <main>
+
