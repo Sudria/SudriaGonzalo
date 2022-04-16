717379de893c3d275f4fe1c97fbe7699e757afb4
Sudria <sudria.gonzalo@gmail.com>
2022-04-16 18:48:26 -0300

    terminado la pagina principal sin responsive

--- app/Views/mains/principal.php
+++ app/Views/mains/principal.php
@@ -1,7 +1,7 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-12 " id="principal">
-            <h1 class=" text-center my-5 text-light" id="titulo"> ESPINNITAS</h1>
+            <h1 class=" text-center my-5 text-light titulo"> ESPINNITAS</h1>
             <p class="text-light text-center mb-5" id="especialistas"> ESPECIALISTAS EN PLANTAS</p>
             <div class="d-flex justify-content-center mb-5">
                 <button class="btn boton-marron ">Comprar online</button>
@@ -9,20 +9,77 @@
         </div>
     </div>
 </div>
-<div class="container">
+<div class="container fondo-gris">
+    <div class="row  justify-content-center">
+        <div class="col-4">
+            <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active"
+                        aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1"
+                        aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2"
+                        aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active" data-bs-interval="10000">
+                        <img src="img/suculenta.jpg" class="d-block w-100 img-fluid rounded mx-auto" alt="suculenta">
+                        <div class="carousel-caption d-none d-md-block">
+                            <button class="boton-marron">Ver mas</button>
+                        </div>
+                    </div>
+                    <div class="carousel-item" data-bs-interval="2000">
+                        <img src="img/cactus.jpg" class="d-block w-100 img-fluid rounded mx-auto" alt="cactus">
+                        <div class="carousel-caption d-none d-md-block">
+                            <button class="boton-marron">Ver mas</button>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img/planta.jpg" class="d-block w-100 img-fluid rounded mx-auto" alt="planta">
+                        <div class="carousel-caption d-none d-md-block">
+                            <button class="boton-marron">Ver mas</button>
+                        </div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark"
+                    data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Anterior</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark"
+                    data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Siguiente</span>
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container fondo-gris">
     <div class="row">
         <div class="col-6 d-flex align-items-center ">
-        <p class="texto-base text-center">
-               <b class="fs-1"> QUIÉNES SOMOS</b> <br>  
+            <p class="texto-base text-center">
+                <b class="fs-1"> QUIÉNES SOMOS</b> <br>
                 ESPINNITAS nace hace más de 10 años en la Ciudad de Corrientes Capital, Argentina.
-                Desde ese momento nos dedicamos al cultivo y asesoramiento profesional en Cactus, Suculentas y Plantas
+                Desde ese momento nos dedicamos al cultivo y asesoramiento profesional en Cactus, Suculentas y
+                Plantas
                 Exóticas.
                 En la actualidad contamos con tres invernáculos donde producimos nuestras plantas.
-                 </p>
+            </p>
         </div>
-        <div class="col-6">
-            <img src="img/hoja.jpg" class="img-fluid" alt="hoja">
-            <img src="img/cactus1.jpg" class="img-fluid" alt="cactus">
+        <div class="col-6  p-0">
+            <img src="img/hoja.jpg" class="img-fluid p-0" alt="hoja">
+            <img src="img/cactus1.jpg" class="img-fluid p-0" alt="cactus">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col p-0">
+            <img src="img/inicio1.jpg" class="img-fluid ocupar-todo" alt="imagen">
+        </div>
+        <div class="col fondo-verde d-flex align-items-center p-0">
+            <p class="text-light text-center ">
+                <b class="titulo">-10% EN COMPRAS ONLINE</b>
+            </p>
         </div>
     </div>
 </div>
--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,7 +1,38 @@
 </main>
 
 <footer>
+    <div class="container mt-3 fondo-verde">
+        <div class="row text-center justify-content-center">
+            <div class="col-4 text-light pt-5">
+                <h4>TIENDA</h4>
+                <p>
+                    Dirección: Av. Los Rosales 122, 28021 Madrid<br>
+                    Teléfono: 914-123-456<br>
+                    Email: info@misitio.com
+                </p>
+            </div>
+            <div class="col-4 text-light pt-5">
+                <h4> HORARIOS</h4>
+                <p>
+                    Lun-Vie: 7:00 - 22:00<br>
+                    Sábados: 8:00 - 22:00<br>
+                    Domingos: 8:00 - 23:00
+                </p>
+            </div>
+            <div class="col-4 text-light pt-5">
+                <h4> ENLACES</h4>
+                <ul id="enlaces">
+                    <li><a href="#">Principal</a></li>
+                    <li><a href="#">Quienes Somos</a></li>
+                    <li><a href="#">Comercialización</a></li>
+                    <li><a href="#">Contacto</a></li>
+                    <li><a href="#">Términos y Usos</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
 
+    </div>
 </footer>
 <script src="js/bootstrap.bundle.min.js"></script>
 <script src="js/jquery-3.4.1.min.js"></script>
